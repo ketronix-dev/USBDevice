3bf55762cc002fa8f5456a3eb7cd88f8b789fc37
ArtemDadashjants <tfastgame@gmail.com>
2022-05-08 20:54:11 +0300

    fix example

--- Example.cs
+++ Example.cs
@@ -4,11 +4,20 @@ class Example
 {
     static void Main()
     {
+<<<<<<< Updated upstream
         var text = USBDevice.GetList();
         var cap = USBDevice.GetUsbDeviceCapacity("/dev/sdb");
         var model = USBDevice.GetUsbDeviceModel("/dev/sdb");
         Console.WriteLine(text[0]);
         Console.WriteLine(cap);
         Console.WriteLine(model);
+=======
+        List<string> text = USBDevice.GetList();
+        string cap = USBDevice.GetUSBDeviceCapacity("/dev/sdb");
+        string model = USBDevice.GetUSBDeviceModel("/dev/sdb");
+        Console.WriteLine("Device: " + text[0]);
+        Console.WriteLine("Capacity: " + cap);
+        Console.WriteLine("Model: " + model);
+>>>>>>> Stashed changes
     }
 }
